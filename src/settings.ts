57b35e3201f94949249a80e52eedb574c4fc6266
Rbqwow <55343783+Rbqwow@users.noreply.github.com>
2024-04-04 20:42:32 +0800

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,8 +6,8 @@ export default {
   textBaseLine: 0.68,
   horizontalTilt: -0.4,
   paddingX: 10,
-  graphOffset: { X: -15, Y: 0 },
-  text2Offset: { X: 100, Y: 32 },
+  graphOffset: { X: 40, Y: 0 },
+  text2Offset: { X: 360, Y: 32 },
   hollowPath: [
     [284, 136],
     [321, 153],
